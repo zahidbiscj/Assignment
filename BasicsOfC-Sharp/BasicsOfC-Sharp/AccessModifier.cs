6bc98aa9bb2d2601787d8a6743935a90f1eb6a6b
Zahid Ahmed <zahidbiscj@gmail.com>
2019-07-31 11:06:48 +0600

    Sir , this is my assignment

---
+++ BasicsOfC-Sharp/BasicsOfC-Sharp/AccessModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOfC_Sharp
+{
+    public class AccessModifier
+    {
+        public int x;
+        private int y;
+        protected int z;
+        internal int a;
+        public void Example() {
+            int j = x + y + z + a;
+        }
+    }
+
+    public class ForProtected : AccessModifier {
+        public void New() {
+            x = 21;
+            /// y is private , so can't access it from other class
+            z = 22;/// Protected can only get by inherit it 
+            a = 23;
+        }
+    }
+}
---
+++ BasicsOfC-Sharp/BasicsOfC-Sharp/Property.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOfC_Sharp
+{
+    class Property
+    {
+        private string Name;
+        private bool hasNameChanged;
+
+        public void setName(string n) {
+            if (n != null)
+            {
+                Name = n;
+            }
+            else
+                Name = string.Empty;
+            hasNameChanged = true;
+        }
+        public string getName() {
+            if (Name == null) { return string.Empty; }
+            else return Name;
+        }
+
+
+    }
+}
